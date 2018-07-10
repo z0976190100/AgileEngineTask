f1cab69edeae745f2521fbbbe333e2c70f78c0ef
z0976190100 <z0976190100@gmail.com>
2018-07-11 00:39:29 +0300

    init commit

---
+++ src/main/java/com.agileengine.xmlcrawler/JsoupFindByIdSnippet.java
@@ -0,0 +1,55 @@
+package com.agileengine.xmlcrawler;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class JsoupFindByIdSnippet {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(JsoupFindByIdSnippet.class);
+
+    private static String CHARSET_NAME = "utf8";
+
+    public static void main(String[] args) {
+
+        // Jsoup requires an absolute file path to resolve possible relative paths in HTML,
+        // so providing InputStream through classpath resources is not a case
+        String resourcePath = "./samples/startbootstrap-freelancer-gh-pages-cut.html";
+        String targetElementId = "sendMessageButton";
+
+        Optional<Element> buttonOpt = findElementById(new File(resourcePath), targetElementId);
+
+        Optional<String> stringifiedAttributesOpt = buttonOpt.map(button ->
+                button.attributes().asList().stream()
+                        .map(attr -> attr.getKey() + " = " + attr.getValue())
+                        .collect(Collectors.joining(", "))
+        );
+
+        stringifiedAttributesOpt.ifPresent(attrs -> LOGGER.info("Target element attrs: [{}]", attrs));
+    }
+
+    private static Optional<Element> findElementById(File htmlFile, String targetElementId) {
+        try {
+            Document doc = Jsoup.parse(
+                    htmlFile,
+                    CHARSET_NAME,
+                    htmlFile.getAbsolutePath());
+
+            return Optional.of(doc.getElementById(targetElementId));
+
+        } catch (IOException e) {
+            LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
+            return Optional.empty();
+        }
+    }
+
+}
+
